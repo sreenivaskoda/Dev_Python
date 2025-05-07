4663902d7d67038ba7cd9594e8def7ddd6e0cb46
SreenivasKoda <Sreenivaskoda@gmail.com>
2025-05-07 15:14:39 +0530

    Initialize README

--- Assignment5/Task2.py
+++ Assignment5/Task2.py
@@ -8,9 +8,7 @@ Problem Statement: Write a Python program that:
 
 '''
 
-ls = []
-for i in range(1,11):
-    ls.append(i)
+ls = [1,2,3,4,5,6,7,8,9,10]
 print("Original list: ", ls)
 extracted = ls[:5]
 print("Extracted first five elements: ", extracted)
